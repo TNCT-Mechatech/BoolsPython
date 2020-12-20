6da15f4d32e7091054e5580622f2646c5453315c
owner <forgotten_saga_1129@outlook.jp>
2020-12-20 20:06:19 +0900

    [Update] add example program

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import os
-
+import Bools
 if __name__ == "__main__":
     print("start program...")
+    bits = [True,True,False,True,False,True,False,False,True]
+    b = Bools.Bools(len(bits),bits)
+    byte = b._get_bytes()
+    print(bin(byte))
+    b._set_bytes(byte)
+    byte = b._get_bytes()
+    print(bin(byte))
